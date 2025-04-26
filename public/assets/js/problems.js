719a1b7ea44ebbda5f0d130c8a2b9d5662d9220e
Zephan Sanghani <70251978+zephanrs@users.noreply.github.com>
2025-04-26 15:22:42 -0400

    new problem

--- public/assets/js/problems.js
+++ public/assets/js/problems.js
@@ -3,13 +3,18 @@
 let problems = [
 	{
 		"title": "Pigeonhole Principle",
-		"description": "Pigeonhole principle.",
+		"description": "do pigeons not have claustrophobia?",
 		"latex": String.raw`f:X\to Y,|X|>|Y|\implies \exists x_1\exists x_2(x_1\neq x_2\land f(x_1)=f(x_2))`
 	},
 	{
 		"title": "Law of the Unconscious Statistician",
-		"description": "Law of the unconscious statistician",
+		"description": "I consciously use this.",
 		"latex": String.raw`\mathbb E[g(X)] = \int_{-\infty}^\infty g(x)f_X(x)\mathrm dx`
+	}
+	{
+		"title": "Harshaan's Identity",
+		"description": "i think it would help...",
+		"latex": String.raw`\Pr(A\cup B) = \Pr(A) + \Pr(B) -\Pr(A\cap B)`
 	}
 	
 ];
